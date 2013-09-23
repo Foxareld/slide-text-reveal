d931d3da39dcb4db9130f8915cd3d82d81690cd3
Alfredo Ramos <aramos@thepowertoprovoke.com>
2013-09-23 08:44:19 -0400

    Initial push of files

---
+++ text_slide_reveal-development.js
@@ -0,0 +1,29 @@
+function slideReveal(event) {
+	var element = $(event.target).parent(".reveal_event");
+	var slide_element = $(element).next(".slide_reveal_container");
+
+	if($(slide_element).hasClass("reveal_open")){
+		$(".reveal_open").animate({width:"0"},1000,function(){
+				$(this).height("0");
+				$(this).removeClass("reveal_open");
+			});
+	}
+	else {
+		if($(".reveal_open").length > 0){
+			$(".reveal_open").animate({width:"0"},1000,function(){
+				$(this).height("0");
+				$(this).removeClass("reveal_open");
+			});
+			
+		}
+		
+		var container_width = $(element).parent().innerWidth() - $(element).outerWidth(true);
+		$(slide_element).height($(element).parent().height());
+		if(animating){
+			$(slide_element).animate({width:container_width},1000,function(){
+				$(this).addClass(" reveal_open");
+				animating = false;
+			});
+		}
+	}
+}
